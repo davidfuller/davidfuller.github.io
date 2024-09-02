964a6c724c2ccfee571c83267bbfd1c8a24c8f52
David Fuller <david@muvi2.co.uk>
2024-09-02 13:47:09 +0100

    Redid the Excel / excel

--- operations.js
+++ operations.js
@@ -36,92 +36,97 @@ async function lockColumns(){
   })   
 }
 
-async function unlock(excel){
-  const sheet = excel.workbook.worksheets.getActiveWorksheet();
-  sheet.protection.load('protected');
-  await excel.sync();
-  if (!sheet.protection.protected){
-    console.log("Already unlocked");
-  } else {
-    console.log("Currently locked");
-    sheet.protection.unprotect("")
+async function unlock(){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    sheet.protection.load('protected');
     await excel.sync();
-    console.log("Now not locked");
-  }
-}
-
-async function applyFilter(excel){
-  /*Jade.listing:{"name":"Apply filter","description":"Applies empty filter to sheet"}*/
-  await unlock(excel);
-  const sheet = excel.workbook.worksheets.getActiveWorksheet();
-  const myRange = await getDataRange(excel);
-  sheet.autoFilter.apply(myRange, 0, { criterion1: "*", filterOn: Excel.FilterOn.custom});
-  sheet.autoFilter.clearCriteria();
-  await excel.sync();
-  
-  await lockColumns(excel);
-}
-
-async function removeFilter(excel){
-  const sheet = excel.workbook.worksheets.getActiveWorksheet();
-  sheet.autoFilter.load('enabled')
-  await excel.sync()
-  if (sheet.autoFilter.enabled){
-    console.log("Autofilter enabled")
-    sheet.autoFilter.remove();
-    await excel.sync();
-  } else {
-    console.log("Autofilter not enabled")
-  }
-  
-  await lockColumns(excel);  
-}
-
-async function findScene(excel, offset){
-  
-  const sceneColumn = myColumns.find(x => x.columnName == "Scene").columnNo;
-  const sheet = excel.workbook.worksheets.getActiveWorksheet();
-  const activeCell = excel.workbook.getActiveCell();
-  activeCell.load("rowIndex");
-  activeCell.load(("columnIndex"))
-  const endRow = sheet.getUsedRange().getLastRow();
-  endRow.load("rowindex");
-  await excel.sync()
-  const startRow = activeCell.rowIndex;
-  const startColumn = activeCell.columnIndex
-  let range;
-  if (offset < 0){
-    range = sheet.getRangeByIndexes(2, sceneColumn, startRow - 1, 1);
-  } else {
-    range = sheet.getRangeByIndexes(startRow, sceneColumn, endRow.rowIndex - startRow, 1);
-  }
-  await excel.sync();
-  
-  range.load("values");
-  await excel.sync();
-  
-  console.log(range.values);
-  let currentValue;
-  if (offset < 0){
-    currentValue = range.values[range.values.length-1][0];
-  } else {
-    currentValue = range.values[0][0];
-  }
-   
-  console.log(currentValue);
-  const myIndex = range.values.findIndex(a => a[0] == (currentValue + offset));
-  console.log(myIndex + startRow);
-  console.log(startColumn);
-  if (myIndex == -1){
-    if (offset == 1){
-      alert('This is the final scene')
+    if (!sheet.protection.protected){
+      console.log("Already unlocked");
+    } else {
+      console.log("Currently locked");
+      sheet.protection.unprotect("")
+      await excel.sync();
+      console.log("Now not locked");
     }
-  } else {
-    const myTarget = sheet.getRangeByIndexes(myIndex + startRow, startColumn, 1, 1);
+  })
+}
+
+async function applyFilter(){
+  /*Jade.listing:{"name":"Apply filter","description":"Applies empty filter to sheet"}*/
+  await Excel.run(async function(excel){
+    await unlock();
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const myRange = await getDataRange(excel);
+    sheet.autoFilter.apply(myRange, 0, { criterion1: "*", filterOn: Excel.FilterOn.custom});
+    sheet.autoFilter.clearCriteria();
     await excel.sync();
-    myTarget.select();
+    await lockColumns();
+  })
+}
+
+async function removeFilter(){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    sheet.autoFilter.load('enabled')
+    await excel.sync()
+    if (sheet.autoFilter.enabled){
+      console.log("Autofilter enabled")
+      sheet.autoFilter.remove();
+      await excel.sync();
+    } else {
+      console.log("Autofilter not enabled")
+    }
+    await lockColumns();
+  })
+}
+
+async function findScene(offset){
+  await Excel.run(async function(excel){
+    const sceneColumn = myColumns.find(x => x.columnName == "Scene").columnNo;
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const activeCell = excel.workbook.getActiveCell();
+    activeCell.load("rowIndex");
+    activeCell.load(("columnIndex"))
+    const endRow = sheet.getUsedRange().getLastRow();
+    endRow.load("rowindex");
+    await excel.sync()
+    const startRow = activeCell.rowIndex;
+    const startColumn = activeCell.columnIndex
+    let range;
+    if (offset < 0){
+      range = sheet.getRangeByIndexes(2, sceneColumn, startRow - 1, 1);
+    } else {
+      range = sheet.getRangeByIndexes(startRow, sceneColumn, endRow.rowIndex - startRow, 1);
+    }
     await excel.sync();
-  }
+    
+    range.load("values");
+    await excel.sync();
+    
+    console.log(range.values);
+    let currentValue;
+    if (offset < 0){
+      currentValue = range.values[range.values.length-1][0];
+    } else {
+      currentValue = range.values[0][0];
+    }
+    
+    console.log(currentValue);
+    const myIndex = range.values.findIndex(a => a[0] == (currentValue + offset));
+    console.log(myIndex + startRow);
+    console.log(startColumn);
+    if (myIndex == -1){
+      if (offset == 1){
+        alert('This is the final scene')
+      }
+    } else {
+      const myTarget = sheet.getRangeByIndexes(myIndex + startRow, startColumn, 1, 1);
+      await excel.sync();
+      myTarget.select();
+      await excel.sync();
+    }
+  })
 }
 
 
