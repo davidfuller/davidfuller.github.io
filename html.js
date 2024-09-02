1ef87989c750fd3e7224946d90df5439073ca6a8
David Fuller <david@muvi2.co.uk>
2024-09-02 17:25:16 +0100

    Added input

--- html.js
+++ html.js
@@ -3,7 +3,9 @@ async function mainHTML(){
                 <button id='btnLock' onclick="jade_modules.operations.lockColumns()">Lock sheet</button>
                 <button id='btnUnlock' onclick="jade_modules.operations.unlock()">Unlock sheet</button>
                 <button id='btnNext' onclick="jade_modules.operations.findScene(1)">Next scene</button>
-                <button id='btnPrev' onclick="jade_modules.operations.findScene(-1)">Previous scene</button>`;
+                <button id='btnPrev' onclick="jade_modules.operations.findScene(-1)">Previous scene</button><br/>
+                <label for="scene">Jump to scene</label>
+                <input type="text" id="scene" name="scene">`;
 
   Jade.open_canvas("Script Controller", html, true);
 }
