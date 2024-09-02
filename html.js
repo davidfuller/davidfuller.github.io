1228982fa35fe39330cbfe5a677998eaa4c86557
David Fuller <david@muvi2.co.uk>
2024-09-02 14:01:15 +0100

    Updated html.js

--- html.js
+++ html.js
@@ -1,15 +1,14 @@
-function mainHTML(){
+async function mainHTML(){
   const html = [];
 
   html.push(`<h1>MuVi2 Script Controller</h1>`);
   html.push(`<button id='btnLock' onclick="jade_modules.operations.lockColumns()">Lock sheet</button>`);
-  html.push(``);
+  html.push(`<button id='btnLock' onclick="jade_modules.operations.unlock()">Unlock sheet</button>`);
   html.push(``);
   html.push(``);
   html.push(``);
 
   html.join(" ");
   Jade.open_canvas("Script Controller", html, true);
-
 }
 
