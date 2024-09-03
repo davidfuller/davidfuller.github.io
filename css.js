830c4769b87d23be8b26d9872fd78dddd7ce92f7
David Fuller <david@muvi2.co.uk>
2024-09-03 16:33:58 +0100

    More styling

--- css.js
+++ css.js
@@ -1,42 +1,59 @@
 async function mainCSS(){
   const css =`body {
-              background-color: #ffc901;
-              color: #d34c01;
-              font-family: 'Aptos Narrow', 'Arial Narrow'Arial, Helvetica, sans-serif;
-            }
-            button {
-              margin-left: 30px;
-              margin-bottom: 5px;
-              margin-top: 5px;
-              padding-left: 15px;
-              padding-right: 15px;
-              height: 36px;
-              border: none;
-              width: 150px;
-              border-radius: 7px;
-              background-color: #d34c01;
-              color: #fef3df;
-              font-size: 16px;
-              cursor: pointer;
-            }
-            h1 {
-              margin-left: 30px;
-              font-family: 'Aptos Narrow', 'Arial Narrow'Arial, Helvetica, sans-serif;
-            }
-            label {
-              margin-left: 30px;
-            }
-              
-            #firstButtons {
-              margin-left: 30px;
-              margin-top: 10px;
-              width: 80%;
-              padding: 10px;
-              border-width: 1px;
-              border-radius: 5px;
-              border-color: #d34c01;
-              border-style: solid;
-            }`;
+                      background-color: #ffc901;
+                      color: #d34c01;
+                      font-family: 'Aptos Narrow', 'Arial Narrow'Arial, Helvetica, sans-serif;
+                    }
+                    button {
+                      margin-left: 30px;
+                      margin-bottom: 5px;
+                      margin-top: 5px;
+                      padding-left: 15px;
+                      padding-right: 15px;
+                      height: 36px;
+                      border: none;
+                      width: 150px;
+                      border-radius: 7px;
+                      background-color: #d34c01;
+                      color: #fef3df;
+                      font-size: 16px;
+                      cursor: pointer;
+                    }
+                    h1 {
+                      margin-left: 30px;
+                      font-family: 'Aptos Narrow', 'Arial Narrow'Arial, Helvetica, sans-serif;
+                    }
+                    label {
+                      margin-left: 30px;
+                    }
+                      
+                    #firstButtons {
+                      margin-left: 30px;
+                      margin-top: 10px;
+                      width: 370px;
+                      padding: 10px;
+                      border-width: 1px;
+                      border-radius: 5px;
+                      border-color: #d34c01;
+                      border-style: solid;
+                    }
+
+                     #jump {
+                      margin-left: 30px;
+                      margin-top: 10px;
+                      width: 370px;
+                      padding: 10px;
+                      border-width: 1px;
+                      border-radius: 5px;
+                      border-color: #d34c01;
+                      border-style: solid;
+                    }
+
+                    #btnGo {
+                      width: fit-content;
+                      height: fit-content;
+                      margin-left: 5px;
+                    }`;
   Jade.set_css(css);
 
 }
