d3f45fcdc6422ea2ebd6f74c932e3cfe355c1739
David Fuller <david@muvi2.co.uk>
2024-09-03 16:50:52 +0100

    More style

--- css.js
+++ css.js
@@ -60,6 +60,10 @@ async function mainCSS(){
                       background-color: #ffc901;
                       color: #d34c01;
                       font-family: 'Aptos Narrow', 'Arial Narrow'Arial, Helvetica, sans-serif;
+                      border-width: 1px;
+                      border-radius: 5px;
+                      border-color: #d34c01;
+                      border-style: solid;
                     }`;
   Jade.set_css(css);
 
