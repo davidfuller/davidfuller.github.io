a51844843142b65debab9827661e558708855451
David Fuller <david@muvi2.co.uk>
2024-09-05 13:48:11 +0100

    dev version

---
+++ operations_dev.js
@@ -0,0 +1,359 @@
+function auto_exec(){
+  console.log('Operations loaded');
+  console.log(jade_modules)
+}
+const columnsToLock = "A:Y"
+const myColumns = 
+  [
+    {
+      columnName: "Scene",
+      columnNo: 77
+    },
+    {
+      columnName: "Line",
+      columnNo: 78
+    },
+    {
+      columnName: "UK Date Recorded",
+      columnNo: 21
+    },
+    {
+      columnName: "UK Studio",
+      columnNo: 22
+    },
+    {
+      columnName: "UK Engineer",
+      columnNo: 23
+    },
+    {
+      columnName: "US Date Recorded",
+      columnNo: 26
+    },
+    {
+      columnName: "US Studio",
+      columnNo: 27
+    },
+    {
+      columnName: "US Engineer",
+      columnNo: 28
+    },
+    {
+      columnName: "Walla Date Recorded",
+      columnNo: 44
+    },
+    {
+      columnName: "Walla Studio",
+      columnNo: 45
+    },
+    {
+      columnName: "Walla Engineer",
+      columnNo: 46
+    }
+  ];
+
+  /*
+const sceneInput = tag("scene");
+sceneInput.onkeydown = function(event){
+  if(event.key === 'Enter'){
+    alert(sceneInput.value)
+  }
+}
+*/
+
+
+async function lockColumns(){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    var range = sheet.getRange(columnsToLock);
+    
+    sheet.protection.load('protected');
+    await excel.sync();
+    
+    console.log(sheet.protection.protected);
+    if (!sheet.protection.protected){
+      console.log("Not locked");
+      range.format.protection.locked = true;
+      sheet.protection.protect({ selectionMode: "Normal", allowAutoFilter: true });
+      await excel.sync();
+      console.log("Now locked");
+    } else {
+      console.log("Locked");
+    }
+  })   
+}
+
+async function unlock(){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    sheet.protection.load('protected');
+    await excel.sync();
+    if (!sheet.protection.protected){
+      console.log("Already unlocked");
+    } else {
+      console.log("Currently locked");
+      sheet.protection.unprotect("")
+      await excel.sync();
+      console.log("Now not locked");
+    }
+  })
+}
+
+async function applyFilter(){
+  /*Jade.listing:{"name":"Apply filter","description":"Applies empty filter to sheet"}*/
+  await Excel.run(async function(excel){
+    await unlock();
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const myRange = await getDataRange(excel);
+    sheet.autoFilter.apply(myRange, 0, { criterion1: "*", filterOn: Excel.FilterOn.custom});
+    sheet.autoFilter.clearCriteria();
+    await excel.sync();
+    await lockColumns();
+  })
+}
+
+async function removeFilter(){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    sheet.autoFilter.load('enabled')
+    await excel.sync()
+    if (sheet.autoFilter.enabled){
+      console.log("Autofilter enabled")
+      sheet.autoFilter.remove();
+      await excel.sync();
+    } else {
+      console.log("Autofilter not enabled")
+    }
+    await lockColumns();
+  })
+}
+
+async function findScene(offset){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const activeCell = excel.workbook.getActiveCell();
+    activeCell.load("rowIndex");
+    activeCell.load(("columnIndex"))
+    await excel.sync()
+    const startRow = activeCell.rowIndex;
+    const startColumn = activeCell.columnIndex
+    let range = await getSceneRange(excel);
+    range.load("values");
+    await excel.sync();
+    console.log("Scene range");
+    console.log(range.values);
+    
+    console.log("Start Row");
+    console.log(startRow);
+
+    let currentValue = range.values[startRow - 2][0];
+    console.log("Current Value");
+    console.log(currentValue);
+
+    let myIndex = -1;
+
+    if (currentValue + offset > 0){
+      myIndex = range.values.findIndex(a => a[0] == (currentValue + offset));
+    }
+    
+    console.log("Found Index");
+    console.log(myIndex);
+    
+    if (myIndex == -1){
+      if (offset == 1){
+        alert('This is the final scene')
+      } else if (offset == -1){
+        await firstScene();
+      }
+    } else {
+      const myTarget = sheet.getRangeByIndexes(myIndex + 2, startColumn, 1, 1);
+      await excel.sync();
+      myTarget.select();
+      await excel.sync();
+    }
+  })
+}
+
+async function findSceneNo(sceneNo){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const activeCell = excel.workbook.getActiveCell();
+    activeCell.load("rowIndex");
+    activeCell.load(("columnIndex"))
+    await excel.sync()
+    const startRow = activeCell.rowIndex;
+    const startColumn = activeCell.columnIndex
+    let range = await getSceneRange(excel);
+    range.load("values");
+    await excel.sync();
+    console.log("Scene range");
+    console.log(range.values);
+    
+    console.log("Start Row");
+    console.log(startRow);
+
+    const minAndMax = await getSceneMaxAndMin();
+    console.log("Min and Max");
+    console.log(minAndMax);
+
+    if (sceneNo > minAndMax.max){
+      sceneNo = minAndMax.max;
+    }
+
+    if (sceneNo < minAndMax.min){
+      sceneNo = minAndMax.min
+    }
+
+    const myIndex = range.values.findIndex(a => a[0] == (sceneNo));
+
+    console.log("Found Index");
+    console.log(myIndex);
+    
+    if (myIndex == -1){
+      alert('Invalid Scene Number');
+    } else {
+      const myTarget = sheet.getRangeByIndexes(myIndex + 2, startColumn, 1, 1);
+      await excel.sync();
+      myTarget.select();
+      await excel.sync();
+    }
+  })
+}
+
+async function firstScene(){
+  await Excel.run(async function(excel){
+    const minAndMax = await getSceneMaxAndMin();
+    console.log("Min and Max");
+    console.log(minAndMax);
+    await findSceneNo(minAndMax.min);
+  })
+}
+
+async function lastScene(){
+  await Excel.run(async function(excel){
+    const minAndMax = await getSceneMaxAndMin();
+    console.log("Min and Max");
+    console.log(minAndMax);
+    await findSceneNo(minAndMax.max);
+  })
+}
+
+
+async function getSceneRange(excel){
+  const sceneColumn = myColumns.find(x => x.columnName == "Scene").columnNo;
+  const sheet = excel.workbook.worksheets.getActiveWorksheet();
+  const endRow = sheet.getUsedRange().getLastRow();
+  endRow.load("rowindex");
+  await excel.sync();
+  range = sheet.getRangeByIndexes(2, sceneColumn, endRow.rowIndex, 1);
+  await excel.sync();
+  return range;
+}
+
+async function getDataRange(excel){
+  const sheet = excel.workbook.worksheets.getActiveWorksheet();
+  const myLastRow = sheet.getUsedRange().getLastRow();
+  const myLastColumn = sheet.getUsedRange().getLastColumn();
+  myLastRow.load("rowindex");
+  myLastColumn.load("columnindex")
+  await excel.sync();
+  
+  const range = sheet.getRangeByIndexes(1,0, myLastRow.rowIndex, myLastColumn.columnIndex + 1);
+  await excel.sync();
+  
+  return range
+}
+
+async function getTargetSceneNumber(){
+  const textValue = tag("scene").value;
+  const sceneNumber = parseInt(textValue);
+  if (sceneNumber != NaN){
+    console.log(sceneNumber);
+    await findSceneNo(sceneNumber);
+  }  else {
+    alert("Please enter a number")
+  }  
+}
+
+async function getSceneMaxAndMin(){
+  let result = {};
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const min = sheet.getRange("minScene");
+    await excel.sync();
+    min.load("values");
+    await excel.sync();
+    const max = sheet.getRange("maxScene");
+    await excel.sync();
+    max.load("values")
+    await excel.sync();
+    console.log(min.values[0][0]);
+    console.log(max.values[0][0]);
+    
+    result.min = min.values[0][0];
+    result.max = max.values[0][0];
+    console.log(result);
+  })
+  return result;
+}
+
+async function fill(country){
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getActiveWorksheet();
+    const studioText = tag("studio").value;
+    const engineerText = tag("engineer").value;
+    let dateColumn;
+    let studioColumn;
+    let engineerColumn;
+    if (country == 'UK'){
+      dateColumn = myColumns.find(x => x.columnName == "UK Date Recorded").columnNo;
+      studioColumn = myColumns.find(x => x.columnName == "UK Studio").columnNo;
+      engineerColumn = myColumns.find(x => x.columnName == "UK Engineer").columnNo;
+    } else if ( country == 'US'){
+      dateColumn = myColumns.find(x => x.columnName == "US Date Recorded").columnNo;
+      studioColumn = myColumns.find(x => x.columnName == "US Studio").columnNo;
+      engineerColumn = myColumns.find(x => x.columnName == "US Engineer").columnNo;
+    } else if ( country == 'Walla'){
+      dateColumn = myColumns.find(x => x.columnName == "Walla Date Recorded").columnNo;
+      studioColumn = myColumns.find(x => x.columnName == "Walla Studio").columnNo;
+      engineerColumn = myColumns.find(x => x.columnName == "Walla Engineer").columnNo;
+    }
+    
+    const activeCell = excel.workbook.getActiveCell();
+    activeCell.load("rowIndex");
+    await excel.sync();
+    const myRow = activeCell.rowIndex;    
+    console.log("Row Index");
+    console.log(myRow);
+    const dateRange = sheet.getRangeByIndexes(myRow, dateColumn, 1, 1);
+    const studioRange = sheet.getRangeByIndexes(myRow, studioColumn, 1, 1);
+    const engineerRange = sheet.getRangeByIndexes(myRow, engineerColumn, 1, 1);
+    await excel.sync();
+    await unlock();
+    console.log(studioRange);
+    dateRange.values = [[dateInFormat()]];
+    studioRange.values = [[studioText]];
+    engineerRange.values = [[engineerText]];
+    await excel.sync();
+    await lockColumns();
+    dateRange.select();
+    await excel.sync();
+  })
+}
+
+function dateInFormat(){
+	var nowDate = new Date(); 
+  let myMonth = (nowDate.getMonth()+1)
+  if (myMonth < 10){
+    myMonth = "0" + myMonth;
+  } else {
+    myMonth = myMonth.toString();
+  }
+  let myDay = nowDate.getDate();
+  if (myDay < 10){
+    myDay = "0" + myDay;
+  } else {
+    myDay = myDay.toString();
+  }
+
+	return nowDate.getFullYear().toString().substring(2) + myMonth + myDay; 
+}
