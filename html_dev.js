ada284511f208ea6cc4a25aa8be6449370a782dd
David Fuller <david@muvi2.co.uk>
2024-09-06 15:14:56 +0100

    Update html_dev.js Added engineer-select

--- html_dev.js
+++ html_dev.js
@@ -19,16 +19,17 @@ async function mainHTML(){
                   <input type="text" id="scene" name="scene">
                   <button id='btnGo' onclick="jade_modules.operations.getTargetSceneNumber()">Go</button><br/>
                 </div>
-                <div id="dateStudioEngineer">
-                  <label for="studio-select">Studio</label>
-                  <select id="studio-select"><option value="">Please select</option></select>
-                  <br/>
-                  <label for="engineer">Engineer</label>
-                  <input type="text" id="engineer" name="engineer"><br/>
-                  <button id='btnFillUK' onclick="jade_modules.operations.fill('UK')">Fill UK</button>
-                  <button id='btnFillUS' onclick="jade_modules.operations.fill('US')">Fill US</button>
-                  <button id='btnFillWalla' onclick="jade_modules.operations.fill('Walla')">Fill Walla</button><br/>
-                </div>
+                  <div id="dateStudioEngineer">
+                    <label for="studio-select">Studio</label>
+                    <select id="studio-select"><option value="">Please select</option></select>
+                    <br/>
+                    <label for="engineer-select">Engineer</label>
+                    <select id="engineer-select"><option value="">Please select</option></select>
+                    <br/>
+                    <button id='btnFillUK' onclick="jade_modules.operations.fill('UK')">Fill UK</button>
+                    <button id='btnFillUS' onclick="jade_modules.operations.fill('US')">Fill US</button>
+                    <button id='btnFillWalla' onclick="jade_modules.operations.fill('Walla')">Fill Walla</button><br/>
+                  </div>
                 `;
 
   Jade.open_canvas("Script Controller", html, true);
