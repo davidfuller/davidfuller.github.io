b053155a0e14badb0213ec6701a1ef26f1c0ad78
David Fuller <davidfuller@users.noreply.github.com>
2024-09-15 16:48:44 +0100

    Missing =

--- operations_dev.js
+++ operations_dev.js
@@ -491,7 +491,7 @@ async function theFormulas(){
     {
       columnName: "Line Word Count", //BY
       formulaFirst:  0,
-      formulaRest: 'IF(NOT(OR(' + ukTakeNoColumn + firstRestRow + '="",' + ukTakeNoColumn + firstRestRow + '=1)), 0, LEN(TRIM(' + UKScriptColumn + firstRestRow + ')) - LEN(SUBSTITUTE(' + UKScriptColumn + firstRestRow + ', " ", "")) + 1)'
+      formulaRest: '=IF(NOT(OR(' + ukTakeNoColumn + firstRestRow + '="",' + ukTakeNoColumn + firstRestRow + '=1)), 0, LEN(TRIM(' + UKScriptColumn + firstRestRow + ')) - LEN(SUBSTITUTE(' + UKScriptColumn + firstRestRow + ', " ", "")) + 1)'
     },
     {
       columnName: "Scene",
