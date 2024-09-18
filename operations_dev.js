555f8e59ea6309c8d106e074d40d129ca463bb83
David Fuller <david@muvi2.co.uk>
2024-09-18 09:15:12 +0100

    Not sure why I have to do this....

--- operations_dev.js
+++ operations_dev.js
@@ -785,7 +785,7 @@ async function removeTake(country){
                 console.log('currentRowIndex: ', lineDetails.currentRowIndex);
                 console.log('ukMarkUpIndex', ukMarkUpIndex);
                 console.log('Diff: ', (ukEngineerIndex - ukMarkUpIndex));
-                let scriptSheet = excel.workbook.worksheets.getActiveWorksheet();
+                scriptSheet = excel.workbook.worksheets.getItem(scriptSheetName);
                 let clearRange = scriptSheet.getRangeByIndexes(lineDetails.currentRowIndex, ukMarkUpIndex, 1, (ukEngineerIndex - ukMarkUpIndex));
                 console.log(clearRange);
                 clearRange.load('address');
