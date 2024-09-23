60f93f06c729ae8c2ec723c0265f3d48d4fa35a5
David Fuller <david@muvi2.co.uk>
2024-09-23 14:45:10 +0100

    Trying no . 3

--- html_dev.js
+++ html_dev.js
@@ -26,6 +26,10 @@ async function mainHTML(){
   <button id='btnShowUK' onclick="jade_modules.operations.showHideColumns('UK Script')">UK Script</button>
   <button id='btnShowUS' onclick="jade_modules.operations.showHideColumns('US Script')">US Script</button>
   <button id='btnShowWalla' onclick="jade_modules.operations.showHideColumns('Walla Script')">Walla Script</button>
+  <label class="container3">One
+    <input type="checkbox" checked="checked">
+    <span class="checkmark3"></span>
+  </label>
   <label class="container2">Always hide UK Script Unedited
     <input type="checkbox" id="alwaysHide" checked="checked">
     <span class="checkmark2"></span>
