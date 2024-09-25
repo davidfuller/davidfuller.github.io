0f3d64b7719b45c74a5b8785db1c0514a21388eb
David Fuller <david@muvi2.co.uk>
2024-09-25 12:16:36 +0100

    Changed it to a format

--- operations_dev.js
+++ operations_dev.js
@@ -1754,10 +1754,11 @@ async function setDefaultColumnWidths(){
       if (mySheetColumns[i].width != ''){
         let myColumn = scriptSheet.getRange(mySheetColumns[i].column + ':' + mySheetColumns[i].column);
         myColumn.load('address')
-        myColumn.load('columnWidth')
+        let myFormat = myColumn.format
+        myFormat.load('columnWidth')
         await excel.sync();
-        console.log(myColumn.address, myColumn.columnWidth);
-        myColumn.columnWidth = mySheetColumns[i].width
+        console.log(myColumn.address, myFormat.columnWidth);
+        myFormat.columnWidth = mySheetColumns[i].width
         console.log(i, mySheetColumns[i].column, mySheetColumns[i].width);
         await excel.sync();
       }
