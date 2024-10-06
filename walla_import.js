af055718348ba74b54536d72b214f226a4a77614
David Fuller <david@muvi2.co.uk>
2024-10-06 12:05:11 +0100

    Find more positions of

--- walla_import.js
+++ walla_import.js
@@ -25,6 +25,8 @@ function splitLine(theLine){
   theCharacter = theSections[0].trim();
   console.log(theCharacter)
   thePosition = theSections[1].trim()
-  console.log(parseInt(thePosition));
-  
+  wholeScene = thePosition.indexOf('whole scene')
+  firstLine = thePosition.indexOf('line')
+  console.log(thePosition, parseInt(thePosition), wholeScene, firstLine);
+
 }
