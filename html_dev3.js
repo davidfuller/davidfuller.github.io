82c772c755b5806362f90fb554549ea86680dfde
David Fuller <david@muvi2.co.uk>
2024-10-08 10:03:00 +0100

    Dev 3 initial commit

---
+++ html_dev3.js
@@ -0,0 +1,50 @@
+async function auto_exec(){
+  console.log("The very beginning");
+  await Jade.load_js("https://davidfuller.github.io/operations_dev2.js", "operations");
+  await Jade.load_js("https://davidfuller.github.io/scheduling.js", "scheduling");
+  await Jade.load_js("https://davidfuller.github.io/walla_import.js", "wallaimport");
+  await Jade.load_js("https://davidfuller.github.io/html_dev2.js", "html");
+  await Jade.load_js("https://davidfuller.github.io/css_dev2.js", "css");
+  await jade_modules.css.mainCSS();
+  await jade_modules.html.mainHTML();
+}
+
+async function openUI(excel){
+  /*Jade.listing:{"name":"Open UI","description":"Opens the user interface"}*/
+  await jade_modules.css.mainCSS();
+  await jade_modules.html.mainHTML();
+}
+
+async function lockColumns(excel){
+  /*Jade.listing:{"name":"Lock columns","description":"This locks columns"}*/
+  await jade_modules.operations.lockColumns();
+}
+
+async function unlock(excel){
+  /*Jade.listing:{"name":"Unprotect sheet","description":"This unlocks sheet"}*/
+  await jade_modules.operations.unlock();
+}
+
+async function applyFilter(excel){
+  /*Jade.listing:{"name":"Apply filter","description":"Applies empty filter to sheet"}*/
+  await jade_modules.operations.applyFilter();
+}
+
+async function removeFilter(excel){
+  /*Jade.listing:{"name": "Remove filter","description":"Removes filter from sheet"}*/
+  await jade_modules.operations.removeFilter();
+}
+
+async function findNextScene(excel){
+  /*Jade.listing:{"name": "Find Next Scene","description":"Finds the start of the next scene"}*/
+  await jade_modules.operations.findScene(1);
+}
+
+async function findPreviousScene(excel){
+  /*Jade.listing:{"name": "Find Previous Scene","description":"Finds the start of the previous scene"}*/
+  await jade_modules.operations.findScene(-1);
+}
+
+function test(){
+  alert('test');
+}
