b56a0a74e23ed480b8793cdd60db53aa141c87fd
David Fuller <david@muvi2.co.uk>
2024-10-17 16:31:15 +0100

    Try to fill the values

--- characterData.js
+++ characterData.js
@@ -1,9 +1,16 @@
 const linkedDataSheetName = 'Linked_Data'
-
+function auto_exec(){
+  console.log('Hello');
+}
 
 async function makeTheFullList(){
   await Excel.run(async function(excel){ 
     let linkedDataSheet = excel.workbook.worksheets.getItem(linkedDataSheetName);
+    let resultRange = linkedDataSheet.getRange('ldAllResults');
+    resultRange.clear("Contents");
+    resultRange.load('rowIndex, columnIndex');
+    await excel.sync();
+    let startRow = resultRange.rowIndex
     for (let i = 1; i<= 7; i++){
       let rangeName = 'ldSheet' + i;
       let thisRange = linkedDataSheet.getRange(rangeName);
@@ -13,6 +20,10 @@ async function makeTheFullList(){
       let filteredValues = myValues.filter((x) => x != 0)
       console.log(i, myValues, filteredValues);
       //let myIndecies = myData.map((x, i) => [x, i]).filter(([x, i]) => x == targetValue).map(([x, i]) => i + firstDataRow - 1);
+      let tempRange = linkedDataSheet.getRangeByIndexes(startRow, resultRange.columnIndex, filteredValues.length, 1);
+      tempRange.values = filteredValues;
+      await excel.sync();
+      let startRow = startRow + filteredValues.length
     }
   })
 }
