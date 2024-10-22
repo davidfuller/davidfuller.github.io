0bbdd5e4c50efad8e29f34659e167ebeb20c076f
David Fuller <david@muvi2.co.uk>
2024-10-22 20:31:22 +0100

    Why 000

--- scheduling_dev6.js
+++ scheduling_dev6.js
@@ -484,7 +484,7 @@ async function processCharacterListForWordAndScene(){
     await excel.sync();
     let myCharacters = characterRange.values.map(x => x[0]);
     console.log('Characters: ', myCharacters, 'rowIndex: ', characterRange.rowIndex )
-    for (let i = 0; i < myCharacters.length; i ++){
+    for (let i = 0; i < 1; i ++){
       if (myCharacters[i] != ''){
         let details = await getWordCountForCharacter(myCharacters[i]);
         console.log(i, 'Character: ', myCharacters[i], ' Details: ', details);
