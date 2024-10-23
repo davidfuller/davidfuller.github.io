13b95eac396260d1c2ed1532b2de3ed9f3a06b0e
David Fuller <david@muvi2.co.uk>
2024-10-23 16:47:36 +0100

    Producing an array

--- characterData.js
+++ characterData.js
@@ -238,6 +238,7 @@ async function gatherData(){
     resultRange.clear("Contents")
     await excel.sync();
     console.log('address: ', resultRange.address);
+    let newRows = [];
     for (let i = 0; i < numBooks; i++){
       let bookName = bookNameBase + (i + 1);
       // Get the book details
@@ -248,7 +249,7 @@ async function gatherData(){
       console.log ('Book: ', i, 'rowCount:', bookRange.rowCount, 'data: ', bookRange.text, 'length', bookRange.text.length);
       resultRange.load('values, rowIndex, rowCount')
       await excel.sync();
-      console.log ('result rowCount', resultRange.rowCount), resultRange.values;
+      console.log ('result rowCount', resultRange.rowCount, 'values: ', resultRange.values);
       let currentNames = resultRange.values.map(x => x[0]).filter((x) => {x != '' })
       console.log(currentNames);
       for (let item = 0; item < bookRange.text.length; item++){
@@ -259,10 +260,12 @@ async function gatherData(){
           } else {
             let newElement = [thisCharacter, '' + (i + 1), bookRange.text[item][1], bookRange.text[item][2], bookRange.text[item][3]];
             console.log('Item:', item, 'New element: ', newElement)
+            newRows.push(newElement);
           }
           
         }
       }
     }
+    console.log('New rows', newRows);
   })
 }
