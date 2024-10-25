641168ca905636de04a86397befdabd9ea2886b1
David Fuller <david@muvi2.co.uk>
2024-10-25 12:01:29 +0100

    Chunk 1000

--- operations_dev7.js
+++ operations_dev7.js
@@ -2060,7 +2060,7 @@ async function doChunkedFilter(character, sheetName){
     let minAndMax = await getLineNoMaxAndMin();
 
     // set up loop variables
-    let chunkLength = 500;
+    let chunkLength = 1000;
     let startChunk = minAndMax.min;
     let endChunk = startChunk + chunkLength;
 
