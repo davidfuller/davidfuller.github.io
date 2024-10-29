67c1fa0d7c39fd38b39da0dac0e78cef6427757e
David Fuller <david@muvi2.co.uk>
2024-10-29 09:51:54 +0000

    Commented out

--- html_dev7.js
+++ html_dev7.js
@@ -258,7 +258,7 @@ async function mainHTML(){
 
   await Jade.open_canvas("Script-Controller", html, true);
   console.log('Canvas open');
-  await jade_modules.operations.getDataFromSheet('Settings','studioChoice','studio-select');
+  //await jade_modules.operations.getDataFromSheet('Settings','studioChoice','studio-select');
   await jade_modules.operations.getDataFromSheet('Settings','engineerChoice','engineer-select');
   await jade_modules.operations.getColumnData('Settings', 'columnData');
   await jade_modules.operations.initialiseVariables();
