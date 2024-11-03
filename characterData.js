d6d81c8f2b0cf10d63b6d70658cddb47da3bc629
David Fuller <davidfuller@users.noreply.github.com>
2024-11-03 11:37:06 +0000

    light grey

--- characterData.js
+++ characterData.js
@@ -667,7 +667,7 @@ async function gotoMain(){
   let scenePage = tag("scene-page");
   scenePage.style.display = 'none'
   let characterSummary = tag('character-summary');
-  characterSummary.style.backgroundColor = sceneBackgroundColour;
+  characterSummary.style.backgroundColor = '#777777' //sceneBackgroundColour;
   characterSummary.style.color = sceneColour;
   await Excel.run(async function(excel){
     const characterSheet = excel.workbook.worksheets.getItem(characterSheetName);
