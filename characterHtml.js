3b7c75541116afee3e44b743504e9d8fcec5b487
David Fuller <david@muvi2.co.uk>
2024-11-04 08:31:53 +0000

    Some html layout changes

--- characterHtml.js
+++ characterHtml.js
@@ -8,45 +8,64 @@ async function mainHTML(){
   <h1>Please wait...</h1>
 </div>
 <div id="main-page">
-  <button id="btnScene" onclick="jade_modules.characterdata.showScenePage()">Characters in Scenes</button><br/>
-  <button id="btnSearch" onclick="jade_modules.characterdata.doSearch()">Search</button>
-  <label id="wait-message">Please wait...</label><br/>
-  <a id='show-hide' onclick="jade_modules.characterdata.showAdmin()">Show/hide admin</a>
+  <div id="main-navigation">
+    <button id="btnMainMain" onclick="jade_modules.characterdata.gotoMain()">Which character in which book</button>
+    <button id="btnScene" onclick="jade_modules.characterdata.showScenePage()">Characters in Scenes</button><br/>
+  </div>
+  <div id="main-controls">
+    <button id="btnSearch" onclick="jade_modules.characterdata.doSearch()">Search</button>
+    <label id="wait-message">Please wait...</label><br/>
+    <a id='show-hide' onclick="jade_modules.characterdata.showAdmin()">Show/hide admin</a>
+  </div>
 </div>
 <div id="admin">
   <label class="section-label">Admin</label><br/>
-  <button id='btnPrepare' onclick="jade_modules.characterdata.gatherData()">Make the list</button><br/>
+  <button id='btnPrepare' onclick="jade_modules.characterdata.gatherData()">Make the list</button>
   <button id="btnRefresh" onclick="jade_modules.characterdata.refreshLinks()">Refresh links</button>
+  <button id="btnCreateSceneList" onclick="jade_modules.characterdata.createSceneList()">Create Scene List</button>
   <label id="admin-wait-message">Please wait...</label>
 </div>
 <div id="scene-page">
-  <button id="btnMain" onclick="jade_modules.characterdata.gotoMain()">Which character in which book</button><br/>
-  <button id="btnCreateSceneList" onclick="jade_modules.characterdata.createSceneList()">Create Scene List</button>
-  <label id="scene-wait-message">Please wait...</label>
-  <label class="container-books">Book 1
-    <input type="checkbox" id='book-1' checked="checked">
-    <span class="checkmark-books"></span>
-  </label>
-  <label class="container-books">Book 2
-    <input type="checkbox" id='book-2' checked="checked">
-    <span class="checkmark-books"></span>
-  </label><label class="container-books">Book 3
-    <input type="checkbox" id='book-3' checked="checked">
-    <span class="checkmark-books"></span>
-  </label><label class="container-books">Book 4
-    <input type="checkbox" id='book-4' checked="checked">
-    <span class="checkmark-books"></span>
-  </label><label class="container-books">Book 5
-    <input type="checkbox" id='book-5' checked="checked">
-    <span class="checkmark-books"></span>
-  </label><label class="container-books">Book 6
-    <input type="checkbox" id='book-6' checked="checked">
-    <span class="checkmark-books"></span>
-  </label><label class="container-books">Book 7
-    <input type="checkbox" id='book-7' checked="checked">
-    <span class="checkmark-books"></span>
-  </label>
+  <div id="scene-navigation">
+    <button id="btnMain" onclick="jade_modules.characterdata.gotoMain()">Which character in which book</button>
+    <button id="btnSceneScene" onclick="jade_modules.characterdata.showScenePage()">Characters in Scenes</button><br/>
+  </div>
+  <div id="scene-controls">
+    <button id="btnCreateSceneList" onclick="jade_modules.characterdata.createSceneList()">Create Scene List</button>
+    <label id="scene-wait-message">Please wait...</label>
+    <div id="book-scheckboxes">
+      <label class="container-books">Book 1
+        <input type="checkbox" id='book-1' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 2
+        <input type="checkbox" id='book-2' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 3
+        <input type="checkbox" id='book-3' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 4
+        <input type="checkbox" id='book-4' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 5
+        <input type="checkbox" id='book-5' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 6
+        <input type="checkbox" id='book-6' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+      <label class="container-books">Book 7
+        <input type="checkbox" id='book-7' checked="checked">
+        <span class="checkmark-books"></span>
+      </label>
+    </div>
+  </div>
 </div>
+
    `;
 
 await Jade.open_canvas("character-summary", html, true);
