74f4057bc79a3561349c3904ec6fb4a3c4565913
David Fuller <david@muvi2.co.uk>
2024-11-07 15:12:21 +0000

    Split it into an object

--- scheduling_dev8.js
+++ scheduling_dev8.js
@@ -893,8 +893,9 @@ async function processCharacterListForWordAndScene(){
     console.log('Characters: ', myCharacters, 'rowIndex: ', characterRange.rowIndex )
     for (let i = 0; i < myCharacters.length; i ++){
       if (myCharacters[i] != ''){
-        console.log(i, myCharacters[i]);
-        let details = await getWordCountForCharacter(myCharacters[i]);
+        let character = {name: myCharacters[i], type: choiceType.list};
+        console.log(i, character);
+        let details = await getWordCountForCharacter(character);
         console.log(i, 'Character: ', myCharacters[i], ' Details: ', details);
         let tempRange = characterListSheet.getRangeByIndexes(i + characterRange.rowIndex, detailsRange.columnIndex, 1, detailsRange.columnCount);
         tempRange.values = [[details.sceneWordCount, details.lineWordCount, details.scenes]];
