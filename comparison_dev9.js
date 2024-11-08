58aab4ffe364c2f5d14985563381850a228a9529
David Fuller <david@muvi2.co.uk>
2024-11-08 16:44:28 +0000

    Try with A

--- comparison_dev9.js
+++ comparison_dev9.js
@@ -77,7 +77,7 @@ async function createChapters(){
     lineRange.clear('Contents');
     lineRange.values = lineValues;
 
-    console.log('Curly opener', myLines[5], findCurlyQuote('‘', myLines[5]));
+    console.log('Curly opener', myLines[5], findCurlyQuote('a', myLines[5]));
 
   })
   
