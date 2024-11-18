b3e17053ff03b796f7637434bd9022ec87b270d0
David Fuller <david@muvi2.co.uk>
2024-11-18 11:41:04 +0000

    Convert to number

--- comparison_dev9.js
+++ comparison_dev9.js
@@ -1005,5 +1005,8 @@ async function autoSelectChapter(){
     chapterRange.load('values');
     await excel.sync();
     console.log('chapter', chapterRange.values[0][0]);
+    const chapter = chapterRange.values[0][0];
+    let chapterNum = jade_modules.wordtonumbers.text2num(chapter);
+    console.log('Chapter Num', chapterNum)
   })
 }
