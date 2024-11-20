9d70e58a2ec853b527b8ae5e5f8b74d79102c45d
David Fuller <david@muvi2.co.uk>
2024-11-20 12:17:07 +0000

    Try to get it started

--- html_dev10.js
+++ html_dev10.js
@@ -313,7 +313,7 @@ async function mainHTML(){
   await jade_modules.operations.getColumnData('Settings', 'columnData');
   await jade_modules.operations.initialiseVariables();
   await jade_modules.operations.displayMinAndMax();
-  await jade_modules.scheduling.loadReduceAndSortCharacters();
+  //await jade_modules.scheduling.loadReduceAndSortCharacters();
   await jade_modules.operations.setDefaultColumnWidths();
   await jade_modules.operations.showHideColumns('all');
   await jade_modules.operations.setUpEvents();
