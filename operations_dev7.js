ea54f0ee5dd497aa3df0f8b3cc50e200351ac3b3
David Fuller <david@muvi2.co.uk>
2024-11-29 12:37:39 +0000

    check character

--- operations_dev7.js
+++ operations_dev7.js
@@ -4223,6 +4223,7 @@ async function getDirectorDataV2(character){
     } else {
       for (let i = 0; i < characterRange.values.length; i++){
         let theValue = '' + characterRange.values[i][0].toLowerCase();
+        console.log('character', character)
         if (theValue == character.name.toLowerCase()){
           index += 1;
           myIndexes[index] = i;
