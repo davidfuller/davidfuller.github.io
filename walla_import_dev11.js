5cd83aea47244e6ef9eb4eaddf8b7a091399d113
David Fuller <david@muvi2.co.uk>
2024-12-18 15:17:04 +0000

    Do the whole scene bit

--- walla_import_dev11.js
+++ walla_import_dev11.js
@@ -147,7 +147,12 @@ function splitLine(theLine){
     }
   }
   let theRestPosition = theLine.toLowerCase().indexOf(thePosition.toLowerCase());
+  if ((lineNo = -1) && (wholeScene != -1){
+    lineNo = 'whole scene'
+  }
+  
   console.log('At this point, theLine', theLine, 'thePosition', thePosition, 'wholeScene', wholeScene, 'firstLine', firstLine, 'lineNo', lineNo, 'theRestPosition', theRestPosition);
+
 
   let theRest = '';
   if (theRestPosition != -1){
