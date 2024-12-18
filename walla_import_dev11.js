0fe9fca40cdb39720ba47af1ebaeb8b97dd96490
David Fuller <david@muvi2.co.uk>
2024-12-18 10:27:36 +0000

    Reduced range to 50 rows

--- walla_import_dev11.js
+++ walla_import_dev11.js
@@ -567,7 +567,7 @@ async function getTheWallaSourceIndecies(){
     usedRange.load('rowIndex, rowCount');
     await excel.sync();
     console.log('rowIndex', usedRange.rowIndex, 'rowCount', usedRange.rowCount);
-    let scriptRange = sourceSheet.getRangeByIndexes(usedRange.rowIndex, wallaSourceUKScriptColumnIndex, usedRange.rowCount, 1)
+    let scriptRange = sourceSheet.getRangeByIndexes(usedRange.rowIndex, wallaSourceUKScriptColumnIndex, 50, 1)// usedRange.rowCount, 1)
     scriptRange.load('values');
     await excel.sync()
     for (let i = 0; i < scriptRange.values.length; i++){
