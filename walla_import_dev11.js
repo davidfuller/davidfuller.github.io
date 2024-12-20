2b57848416d8f2bd1f0b86ad4e150c0fd9a9fd1a
David Fuller <david@muvi2.co.uk>
2024-12-20 07:57:04 +0000

    jade_modules

--- walla_import_dev11.js
+++ walla_import_dev11.js
@@ -1022,5 +1022,5 @@ async function doWallaScripting(){
 }
 async function deleteAllWallaScripting(){
   let indexes = await jade_modules.operations.findAllWallaScripted();
-  await deleteRowsFromIndexes(indexes, true);
+  await jade_modules.operations.deleteRowsFromIndexes(indexes, true);
 }
