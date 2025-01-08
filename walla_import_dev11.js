084c9e217b9375d3d8adab620c22a301825a82d9
David Fuller <david@muvi2.co.uk>
2025-01-08 11:09:47 +0000

    How many!

--- walla_import_dev11.js
+++ walla_import_dev11.js
@@ -59,7 +59,7 @@ const wallaScriptingNames = ['WALLA SCRIPTED', 'WALLA SCRIPTED LINES', 'WALLA SC
 
 const namedCharacters = ['Named Characters - For reaction sounds and walla', 'Named Characters - For reaction sounds and walla:', 'Named Characters Reactions and Walla', 'Named character walla', 'Named - Character & Reactions', 
   'Named character walla:', 'Named character walla', 'Named Characters Reactions and Walla:', 'Named Characters for Reaction Sounds & Walla:', 'Named Characters for reaction sounds and Walla:', 'Named Chsracters for reaction sounds and walla:', 
-  'Named characters for reaction sounds and walla', 'Characters for reaction sounds and walla']
+  'Named characters for reaction sounds and walla', 'Characters for reaction sounds and walla', 'Named characters for. reaction sounds and walla:']
 let displayWallaName = 'Named Characters Reactions and Walla:'
 const unnamedCharacters = ['Un-named Character Walla','Un-named Character Walla:', 'Un-named Character Walla: None', 'Unnamed Character Walla:', 'Un-named characters:', 'Un-named character walla :', 'Un-named character walla : none'];
 let displayWallaUnNamed = 'Un-named Character Walla:';
