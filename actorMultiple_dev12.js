711ba111e81bd59d0bac0fec0fac5bb32c4c0287
David Fuller <david@muvi2.co.uk>
2025-01-09 22:45:56 +0000

    addScript nearly done

--- actorMultiple_dev12.js
+++ actorMultiple_dev12.js
@@ -1,5 +1,9 @@
-const forActorName = 'For Actors'
-const multiActorTableName = 'faMultiActorTable'
+const forActorName = 'For Actors';
+const multiActorTableName = 'faMultiActorTable';
+const typeChoiceName = 'faChoice';
+const textValueName = 'faTextSearch';
+const listValueName = 'faCharacterChoice';
+const allUsName = 'faSelect';
 const multiActorColumns = [
   {name: 'No', column: 0},
   {name: 'Character', column: 1},
@@ -8,7 +12,6 @@ const multiActorColumns = [
   {name: 'Scene', column: 4},
 ]
 
-
 async function auto_exec(){
   console.log('Actor Multiple');
 }
@@ -16,10 +19,11 @@ async function auto_exec(){
 async function addScript(){
   let characterColumn = getColumnNumber('Character');
   let addRowNo = -1
+  let actorDetails = await getActorDetails();
   await Excel.run(async function(excel){
     const sheet = excel.workbook.worksheets.getItem(forActorName);
     const range = sheet.getRange(multiActorTableName);
-    range.load ('values, rowIndex');
+    range.load ('values, rowIndex, columnIndex');
     await excel.sync();
     for (let i = 0; i < range.values.length; i++){
       if (range.values[i][characterColumn] == ''){
@@ -28,8 +32,9 @@ async function addScript(){
       }
     }
     console.log('addRowNo', addRowNo)
-
-
+    let resultRange = sheet.getRangeByIndexes(addRowNo + range.rowIndex, characterColumn + range.columnIndex, 1, 3);
+    let resultArray = [[actorDetails.character, actorDetails.type, actorDetails.allUs]];
+    resultRange.values = resultArray;
   })
 }
 
@@ -42,4 +47,29 @@ function getColumnNumber(theName){
     }
   }
   return result
+}
+
+async function getActorDetails(){
+  let details = {};
+  await Excel.run(async function(excel){
+    const sheet = excel.workbook.worksheets.getItem(forActorName);
+    const typeRange = sheet.getRange(typeChoiceName);
+    const allUsRange = sheet.getRange(allUsName);
+    typeRange.load('values');
+    allUsRange.load('values');
+    await excel.sync();
+    details.type = typeRange.values[0][0];
+    details.allUs = allUsRange.values[0][0];
+    let characterRange;
+    if (details.type == 'Text Search'){
+      characterRange = sheet.getRange(textValueName);
+    } else {
+      characterRange = sheet.getRange(listValueName);
+    }
+    characterRange.load('values');
+    await excel.sync();
+    details.character = characterRange.values[0][0];
+  })
+  console.log('details', details);
+  return details;
 }
