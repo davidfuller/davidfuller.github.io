c1adb8ff84de8315d471c6180192ece255ae9976
David Fuller <david@muvi2.co.uk>
2025-02-06 12:35:57 +0000

    15.02

--- operations_dev15.js
+++ operations_dev15.js
@@ -2,7 +2,7 @@ function auto_exec(){
 }
 
 let doingTake = false;
-const codeVersion = '15.01';
+const codeVersion = '15.02';
 //11.02 isProtected on applyTakeDetails
 //11.03 31-35 in walla and remove the clears
 //11.04 30-35 in walla
