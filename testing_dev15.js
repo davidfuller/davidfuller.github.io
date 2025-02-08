297f79c990f506a59ac6bc3674850d14b125e26a
David Fuller <david@muvi2.co.uk>
2025-02-08 23:12:50 +0000

    try color

--- testing_dev15.js
+++ testing_dev15.js
@@ -595,7 +595,7 @@ async function checkForActorConditionalFormatting(){
       console.log('Doing cell', myFormat.name);
       console.log('mainFont', myFormat.mainFontStyle);
       const range = sheet.getRange(myFormat.name);
-      range.format.fill = myFormat.mainFontStyle.fill;
+      range.format.fill.color = myFormat.mainFontStyle.fill.color;
       range.conditionalFormats.clearAll();
       const conditionalFormat = range.conditionalFormats.add(Excel.ConditionalFormatType.custom);
       conditionalFormat.custom.rule.formula = myFormat.rule;
