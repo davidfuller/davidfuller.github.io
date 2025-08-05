259ecf1ad04cf3f6046a3ee0121fde51e1239a85
David Fuller <david@muvi2.co.uk>
2025-08-05 14:05:45 +0100

    More stats

--- operations_german_01.js
+++ operations_german_01.js
@@ -33,13 +33,54 @@ async function processGerman(){
     await excel.sync();
     let germanText = originalTextRange.values.map(x => x[0]);
     console.log(germanText);
-    for(i = 0; i < germanText.length; i++){
+    let results = []
+    let totalDirectCopy = 0;
+    let totalGood = 0;
+    let totalWrong = 0;
+    let totalUnequal = 0;
+    for(let i = 0; i < germanText.length; i++){
+      let result = {};
       let startQuotes = locations(openSpeechChar, germanText[i])
       let endQuotes = locations(closeSpeechChar, germanText[i])
-      console.log(i, ' - ', startQuotes, ',', endQuotes)
+      let directCopy;
+      let goodSpeech = 0;
+      let wrongSpeech = 0;
+      let unequalQuotes = 0;
+      if (startQuotes.length == endQuotes.length){
+        if (startQuotes.length == 0){
+          directCopy == true
+        } else {
+          directCopy == false
+          for (let speechPart = 0; speechPart < startQuotes.length; speechPart++ ){
+            if (endQuotes(speechPart) > startQuotes(speechPart)){
+              goodSpeech += 1;
+            } else {
+              wrongSpeech += 1;
+            }
+          }
+        }
+      } else {
+        directCopy = false
+        unequalQuotes += 1;
+      }
+      result.directCopy = directCopy;
+      if (directCopy){
+        totalDirectCopy += 1;
+      }
+      result.goodSpeech = goodSpeech;
+      totalGood = totalGood + goodSpeech
+      result.wrongSpeech = wrongSpeech;
+      totalWrong = totalWrong + wrongSpeech
+      result.unequalQuotes = unequalQuotes
+      totalUnequal = totalUnequal + unequalQuotes
+      results.push(result)
+      console.log(i, ' - ', startQuotes, ',', endQuotes, ":", result )
     }
+    console.log('Results')
+    console.log('Total Good', totalGood, 'Total Wrong', totalWrong, 'Total Unequal', totalUnequal)
   })
 }
+
 
 function locations(substring,string){
   var a=[],i=-1;
