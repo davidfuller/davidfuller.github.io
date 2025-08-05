aa376e316c741143b005e705f4c958e7bba33102
David Fuller <david@muvi2.co.uk>
2025-08-05 10:30:03 +0100

    More changes

--- html_german_01.js
+++ html_german_01.js
@@ -7,7 +7,9 @@ async function mainHTML(){
   <h1>Please wait...</h1>
 </div>
 <div id="main-page">
+<h1>Hello Fran and Tom</h1>  
 </div>`;
   await Jade.open_canvas("Script-Controller", html, true);
   console.log('Canvas open');
+  await jade_modules.operations.showMain();
 }
