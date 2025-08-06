c81505784c19f87c38f6483bd99b64715e262e51
David Fuller <david@muvi2.co.uk>
2025-08-06 09:17:48 +0100

    More debug

--- operations_german_01.js
+++ operations_german_01.js
@@ -149,6 +149,7 @@ async function fillRange(sheetName, rangeName, dataArray, doClear){
 function trimEmptyEnd(dataArray){
   console.log('dataArray', dataArray)
   for(let i = dataArray.length - 1; i < 0; i--){
+    console.log(i, dataArray[i], dataArray[i].length)
     if (dataArray[i] != ''){
       console.log('Found first non empty', i)
       return dataArray.slice(0, i);
