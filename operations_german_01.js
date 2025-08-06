04f0b7ebbdbbfab9d3590565e1b24440e10fc076
David Fuller <david@muvi2.co.uk>
2025-08-06 09:23:31 +0100

    Actually do something

--- operations_german_01.js
+++ operations_german_01.js
@@ -140,18 +140,17 @@ async function fillRange(sheetName, rangeName, dataArray, doClear){
 
   const destRange = mySheet.getRangeByIndexes(myRange.rowIndex, myRange.columnIndex, dataArray.length, 1)
   destRange.load('address');
-  await excel.sync()
-  console.log('address:', destRange.address)
+  await excel.sync();
+  console.log('address:', destRange.address);
+  destRange.values = [dataArray];
+  await excel.sync();
 
  }) 
 }
 
 function trimEmptyEnd(dataArray){
-  console.log('dataArray', dataArray)
   for(let i = dataArray.length - 1; i >= 0; i--){
-    console.log(i, dataArray[i], dataArray[i].length)
     if (dataArray[i] != ''){
-      console.log('Found first non empty', i)
       return dataArray.slice(0, i);
     }
   }
