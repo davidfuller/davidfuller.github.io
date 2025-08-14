f5f58dcba0098c08200322a18697a760b664cc68
David Fuller <david@muvi2.co.uk>
2025-08-14 13:44:05 +0100

    Translation

---
+++ translation_german_01.js
@@ -0,0 +1,17 @@
+const germanProcessingSheetName = 'German Processing';
+const translationCacheSheetName = 'Translation Cache';
+const gpTranslationRangeName = 'gpTranslation';
+const tcTranslationRangeName = 'tcTranslation';
+
+async function copyValuesToCache(){
+  await Excel.run(async function(excel) {
+    const gpSheet = excel.workbook.worksheets.getItem(germanProcessingSheetName);
+    let translationRange = gpSheet.getRange(gpTranslationRangeName);
+    const tcSheet = excel.workbook.worksheets.getItem(translationCacheSheetName);
+    let cacheRange = tcSheet.getRange(tcTranslationRangeName);
+    await excel.sync()
+    cacheRange.clear('Contents');
+    cacheRange.copyFrom(translationRange, 'values');
+    await excel.sync()
+  })
+}
