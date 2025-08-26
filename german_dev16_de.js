619634ef539333fed026ffebec30166ac451c3b9
David Fuller <davidfuller@users.noreply.github.com>
2025-08-26 22:19:53 +0100

    getrange

--- german_dev16_de.js
+++ german_dev16_de.js
@@ -27,7 +27,7 @@ async function createScriptNames(){
     console.log('After excel.sync()');
     console.log(scriptRangeNames);
     for (let i = 0; i < scriptRangeNames.length;i++){
-      let tempRange = scriptSheet.getRangeOrNullObject(scriptRangeNames[i].name);
+      let tempRange = excel.workbook.names.getItemOrNullObject(scriptRangeNames[i].name).getRangeOrNullObject();
       await excel.sync();
       console.log(i, 'tempRange', tempRange);
       
