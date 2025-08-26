db542042db8edf61526175b7beec71e212e19e7b
David Fuller <david@muvi2.co.uk>
2025-08-26 18:37:04 +0100

    Debugging

--- german_dev_16_de.js
+++ german_dev_16_de.js
@@ -14,9 +14,12 @@ const scriptRangeNames = [
 ]
 
 async function createScriptNames(){
+  console.log('createSciptNames')
   await Excel.run(async function(excel){
     let scriptSheet = excel.workbook.worksheets.getItem(scriptSheetName);
     await excel.sync();
+    console.log('After excel.sync()');
+    console.log(scriptRangeNames);
     for (let i = 0; i < scriptRangeNames.length;i++){
       let tempRange = workbook.names.getItemOrNullObject(scriptRangeNames[i].name);
       await excel.sync();
