521e128d2c5f7cca43d1741aef183e6d4cc9b0b8
David Fuller <davidfuller@users.noreply.github.com>
2025-09-14 19:25:48 +0100

    do the full loop

--- german_dev16_de.js
+++ german_dev16_de.js
@@ -445,8 +445,8 @@ async function copyToMainScript(){
     let commentDestinationDetails = await getRangeDetails(scriptSheetName, 'scGermanComment');
     //await clearRangeContents(scriptSheetName, 'scGermanComment');
     let scriptSheet = excel.workbook.worksheets.getItem(scriptSheetName);
-    //for (let i = 0; i < scriptSourceDetails.values.length; i++){
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i < scriptSourceDetails.values.length; i++){
+    //for (let i = 0; i < 100; i++){
       germanMessage(true, 'Doing: ' + i + ' of ' + scriptSourceDetails.values.length);
       let theValue = scriptSourceDetails.values[i].trim()
       let theComment = commentSourceDetails.values[i].trim()
