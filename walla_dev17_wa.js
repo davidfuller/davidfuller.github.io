69aac978a9011d861ee1d7aa837d79b24f00d35a
David Fuller <david@muvi2.co.uk>
2025-10-20 14:34:02 +0100

    Indexes

--- walla_dev17_wa.js
+++ walla_dev17_wa.js
@@ -7,7 +7,7 @@ async function getUsedCueRange(){
   let cueRangeAddress = '';
   await Excel.run(async function(excel){
     const scriptSheet = excel.workbook.worksheets.getItem('Script');
-    let fullCueRange = scriptSheet.getRangeByIndex(startRow, cueColumnIndex, maxRowCount, 1);
+    let fullCueRange = scriptSheet.getRangeByIndexes(startRow, cueColumnIndex, maxRowCount, 1);
     let cueRange = fullCueRange.getUsedRange();
     cueRange.load('address');
     await excel.sync();
