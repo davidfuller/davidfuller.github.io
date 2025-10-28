93d84836f5d0a27ba71a704a513317fe6e053853
David Fuller <david@muvi2.co.uk>
2025-10-28 10:10:49 +0000

    Should have been character

--- walla_dev17_wa.js
+++ walla_dev17_wa.js
@@ -408,7 +408,7 @@ function extractWallaScript(characters, possibleWallaText){
     if (stat.num == 1){
       for (let trimmed of trimmedCharacters){
         if (stat.text.toLowerCase().includes(trimmed.character)){
-          let reg = new RegExp(trimmed.text,"ig");
+          let reg = new RegExp(trimmed.character,"ig");
           console.log('Before', stat.text, 'Regex', reg);
           let theText = stat.text.replaceAll(reg, '').trim();
           console.log('script', theText, 'rowIndex', trimmed.rowIndex);
